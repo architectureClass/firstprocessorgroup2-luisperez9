f14e61d65e160dae1d8cd721355f04aa1df94416
luisperez9 <pipefperez@utp.edu.co>
2017-04-21 16:32:13 -0500

    Update ALU.vhd

--- ProcesadorDos/ALU.vhd
+++ ProcesadorDos/ALU.vhd
@@ -25,17 +25,17 @@ begin
 			
 			when "000001" => dwr <= std_logic_vector(signed(crs1) - signed(crs2)); 	--Sub
 			
-			when "000010" => dwr <= crs1 and crs2; 											--And
+			when "000010" => dwr <= crs1 and crs2; 			 		--And
 			
-			when "000011" => dwr <= crs1 and (not(crs2));									--AndN
+			when "000011" => dwr <= crs1 and (not(crs2));				--AndN
 			
-			when "000100" => dwr <= crs1 or crs2;												--Or
+			when "000100" => dwr <= crs1 or crs2;					--Or
 			
-			when "000101" => dwr <= crs1 or (not(crs2));										--OrN
+			when "000101" => dwr <= crs1 or (not(crs2));				--OrN
 			
-			when "000110" => dwr <= crs1 xor crs2;												--XOr
+			when "000110" => dwr <= crs1 xor crs2;					--XOr
 			
-			when "000111" => dwr <= not(crs1 xor crs2);										--XNOr
+			when "000111" => dwr <= not(crs1 xor crs2);				--XNOr
 			
 			when others => dwr <= (others => '0');
 		end case;
